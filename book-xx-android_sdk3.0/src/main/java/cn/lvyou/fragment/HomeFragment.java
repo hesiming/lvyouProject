0aa01146e38d14e4a52722c0cc8cb82b80423f4d
Administrator <Administrator@Norven>
2014-05-04 00:01:39 +0800

    筛选分类的显示效果大致完成

--- book-xx-android_sdk3.0/src/main/java/cn/lvyou/fragment/HomeFragment.java
+++ book-xx-android_sdk3.0/src/main/java/cn/lvyou/fragment/HomeFragment.java
@@ -1,24 +1,31 @@
 package cn.lvyou.fragment;
 
-import cn.retech.my_network_engine.IDomainBeanAsyncHttpResponseListener;
-import cn.retech.my_network_engine.net_error_handle.MyNetRequestErrorBean;
+import android.view.Gravity;
 
-import cn.retech.my_network_engine.IDomainBeanAsyncHttpResponseListener;
-import cn.retech.my_network_engine.net_error_handle.MyNetRequestErrorBean;
-
-import cn.retech.my_network_engine.SimpleNetworkEngineSingleton;
-
-import cn.retech.domainbean_model.categorys.CategorysNetRequestBean;
-
-import cn.retech.my_network_engine.INetRequestHandle;
-import cn.retech.my_network_engine.NetRequestHandleNilObject;
-
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+import android.widget.Toast;
 import cn.lvyou.activity.R;
+import cn.retech.domainbean_model.categorys.CategorysNetRequestBean;
+import cn.retech.domainbean_model.categorys.CategorysNetRespondBean;
+import cn.retech.domainbean_model.categorys.categorybeans.CategoryDate;
+import cn.retech.domainbean_model.categorys.categorybeans.CategoryOriginPlace;
+import cn.retech.domainbean_model.categorys.categorybeans.CategoryPlace;
+import cn.retech.domainbean_model.categorys.categorybeans.CategoryType;
+import cn.retech.my_network_engine.IDomainBeanAsyncHttpResponseListener;
+import cn.retech.my_network_engine.INetRequestHandle;
+import cn.retech.my_network_engine.NetRequestHandleNilObject;
+import cn.retech.my_network_engine.SimpleNetworkEngineSingleton;
+import cn.retech.my_network_engine.net_error_handle.MyNetRequestErrorBean;
 
 /**
  * 主界面
@@ -29,12 +36,98 @@ import cn.lvyou.activity.R;
 public class HomeFragment extends Fragment {
   private INetRequestHandle netRequestHandleForCategroys = new NetRequestHandleNilObject();
 
+  private CategorysNetRespondBean categoryNetRespondBean;
+  private LinearLayout categoryLayout;
+  private PopupWindow categoryPopupWindow;
+  private LinearLayout listViewInCategoryPopupWindow;
+  private TextView categoryTypesTextView;
+  private TextView categoryOriginPlaceTextView;
+  private TextView categoryPlaceTextView;
+  private TextView categoryDatesTextView;
+
+  private OnClickListener onCategoryClickListener = new OnClickListener() {
+    @SuppressLint("ResourceAsColor")
+    @Override
+    public void onClick(View v) {
+      if (null == categoryNetRespondBean) {
+        Toast.makeText(getActivity(), "筛选类型仍在努力加载，请稍后再试!", Toast.LENGTH_LONG).show();
+      }
+
+      if (null != categoryPopupWindow && categoryPopupWindow.isShowing()) {
+        categoryPopupWindow.dismiss();
+
+        if (listViewInCategoryPopupWindow.getTag() != null && (Integer) listViewInCategoryPopupWindow.getTag() == v.getId()) {
+          return;
+        }
+      }
+
+      listViewInCategoryPopupWindow.removeAllViews();
+      listViewInCategoryPopupWindow.setTag(v.getId());
+
+      switch (v.getId()) {
+        case R.id.categoryDatesTextView:
+          for (CategoryDate categoryDate : categoryNetRespondBean.getDates()) {
+            TextView textView = new TextView(getActivity());
+            textView.setTextSize(19f);
+            textView.setGravity(Gravity.CENTER_VERTICAL);
+            textView.setTextColor(getResources().getColor(android.R.color.black));
+            textView.setText(categoryDate.getDescription());
+            listViewInCategoryPopupWindow.addView(textView, new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, 85));
+          }
+          break;
+        case R.id.categoryTypesTextView:
+          for (CategoryType categoryType : categoryNetRespondBean.getTypes()) {
+            TextView textView = new TextView(getActivity());
+            textView.setTextSize(19f);
+            textView.setGravity(Gravity.CENTER_VERTICAL);
+            textView.setTextColor(getResources().getColor(android.R.color.black));
+            textView.setText(categoryType.getCateName());
+            listViewInCategoryPopupWindow.addView(textView, new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, 85));
+          }
+          break;
+        case R.id.categoryOriginPlaceTextView:
+          for (CategoryOriginPlace categoryOriginPlace : categoryNetRespondBean.getOriginPlace()) {
+            TextView textView = new TextView(getActivity());
+            textView.setTextSize(19f);
+            textView.setGravity(Gravity.CENTER_VERTICAL);
+            textView.setTextColor(getResources().getColor(android.R.color.black));
+            textView.setText(categoryOriginPlace.getCityDes());
+            listViewInCategoryPopupWindow.addView(textView, new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, 85));
+          }
+          break;
+        case R.id.categoryPlaceTextView:
+          for (CategoryPlace categoryPlace : categoryNetRespondBean.getPlaces()) {
+            TextView textView = new TextView(getActivity());
+            textView.setTextSize(19f);
+            textView.setGravity(Gravity.CENTER_VERTICAL);
+            textView.setTextColor(getResources().getColor(android.R.color.black));
+            textView.setText(categoryPlace.getContinentName());
+            listViewInCategoryPopupWindow.addView(textView, new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, 85));
+          }
+          break;
+
+      }
+
+      categoryPopupWindow.showAsDropDown(categoryLayout);
+    }
+  };
+
   public HomeFragment() {
   }
 
   @Override
   public void onActivityCreated(Bundle savedInstanceState) {
     super.onActivityCreated(savedInstanceState);
+
+    View popupWindowView = getActivity().getLayoutInflater().inflate(R.layout.categorys_popupwindow_content, null, false);
+    categoryPopupWindow = new PopupWindow(popupWindowView, LayoutParams.MATCH_PARENT, 500, false);
+    listViewInCategoryPopupWindow = (LinearLayout) popupWindowView.findViewById(R.id.categoryContentList);
+
+    categoryLayout = (LinearLayout) getView().findViewById(R.id.categorysLayout);
+    categoryTypesTextView = (TextView) getView().findViewById(R.id.categoryTypesTextView);
+    categoryOriginPlaceTextView = (TextView) getView().findViewById(R.id.categoryOriginPlaceTextView);
+    categoryPlaceTextView = (TextView) getView().findViewById(R.id.categoryPlaceTextView);
+    categoryDatesTextView = (TextView) getView().findViewById(R.id.categoryDatesTextView);
 
     requestCategroys();
   }
@@ -63,7 +156,19 @@ public class HomeFragment extends Fragment {
 
           @Override
           public void onSuccess(Object respondDomainBean) {
-            // TODO Auto-generated method stub
+            categoryNetRespondBean = (CategorysNetRespondBean) respondDomainBean;
+
+            categoryDatesTextView.setText("时间");
+            categoryDatesTextView.setOnClickListener(onCategoryClickListener);
+
+            categoryOriginPlaceTextView.setText("出发地");
+            categoryOriginPlaceTextView.setOnClickListener(onCategoryClickListener);
+
+            categoryPlaceTextView.setText("目的地");
+            categoryPlaceTextView.setOnClickListener(onCategoryClickListener);
+
+            categoryTypesTextView.setText("机票");
+            categoryTypesTextView.setOnClickListener(onCategoryClickListener);
           }
         });
   }
