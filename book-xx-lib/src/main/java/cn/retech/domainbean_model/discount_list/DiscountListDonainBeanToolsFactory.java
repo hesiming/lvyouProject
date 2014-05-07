12b186f5ea57b9063d846617a132e78e33e097e1
hesiming <hesiming1@126.com>
2014-05-07 16:51:30 +0800

    添加折扣列表的URL备注

--- book-xx-lib/src/main/java/cn/retech/domainbean_model/discount_list/DiscountListDonainBeanToolsFactory.java
+++ book-xx-lib/src/main/java/cn/retech/domainbean_model/discount_list/DiscountListDonainBeanToolsFactory.java
@@ -19,6 +19,8 @@ public final class DiscountListDonainBeanToolsFactory implements IDomainBeanAbst
 
 	@Override
 	public String getSpecialPath(Object netRequestDomainBean) {
+		// 折扣列表
+		// public static final String kUrlConstant_SpecialPath_DISCOUNT_LIST = "lastminute/get_lastminute_list";
 		return UrlConstantForThisProject.kUrlConstant_SpecialPath_DISCOUNT_LIST;
 	}
 
