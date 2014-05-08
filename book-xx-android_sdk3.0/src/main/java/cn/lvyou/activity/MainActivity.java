ade261c9fa64f36d88306bf880b98e36514a11fb
zhihuatang <zhihuatang@192.168.1.106>
2014-05-08 15:10:23 +0800

    重构 MainActivity 的代码, 重命名 各布局文件名称

--- book-xx-android_sdk3.0/src/main/java/cn/lvyou/activity/MainActivity.java
+++ book-xx-android_sdk3.0/src/main/java/cn/lvyou/activity/MainActivity.java
@@ -1,12 +1,13 @@
 package cn.lvyou.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.util.Log;
 import android.view.MenuItem;
 import cn.lvyou.fragment.LeftMenuFragment;
+import cn.lvyou.fragment.LeftMenuFragment.LeftMenuIndexEnum;
 import cn.lvyou.fragment.main_left_menu.DiscountFragment;
 import cn.lvyou.fragment.main_left_menu.FavorFragment;
 import cn.lvyou.fragment.main_left_menu.NotificationFragment;
@@ -15,116 +16,151 @@ import cn.lvyou.fragment.main_left_menu.SettingFragment;
 import cn.lvyou.global_data_cache.GlobalDataCacheForMemorySingleton;
 import cn.lvyou.slidingmenu.SlidingMenu;
 import cn.lvyou.slidingmenu.app.SlidingFragmentActivity;
+import cn.lvyou.toolutils.DebugLog;
 
 public class MainActivity extends SlidingFragmentActivity {
-	private final LeftMenuFragment leftMenuFragment = new LeftMenuFragment();
+  private final String TAG = this.getClass().getSimpleName();
+  private final LeftMenuFragment leftMenuFragment = new LeftMenuFragment();
 
-	private void initSlidingMenu() {
-		// 设置背后的视图
-		setBehindContentView(R.layout.frame_left_menu);
+  private static enum IntentRequestCodeEnum {
+    TO_LOGIN_ACTIVITY
+  };
 
-		// 自定义SlidingMenu
-		// 设置只有左侧可以划出,如果需要设置左右都能划LEFT_RIGHT
-		getSlidingMenu().setMode(SlidingMenu.LEFT);
-		// 设置阴影图片
-		getSlidingMenu().setShadowDrawable(R.drawable.bg_sliding_menu_shadow);
-		// 设置阴影图片的宽度
-		getSlidingMenu().setShadowWidthRes(R.dimen.shadow_width);
-		// SlidingMenu划出时主页面显示的剩余宽度
-		getSlidingMenu().setBehindOffsetRes(R.dimen.slidingmenu_offset);
-		// 设置SlidingMenu划出淡入淡出的褪色程度0.0f-1.0f
-		getSlidingMenu().setFadeDegree(0.35f);
-		// 设置SlidingMenu 的手势模式
-		// TOUCHMODE_FULLSCREEN 全屏模式，在整个content页面中，滑动，可以打开SlidingMenu
-		// TOUCHMODE_MARGIN
-		// 边缘模式，在content页面中，如果想打开SlidingMenu,你需要在屏幕边缘滑动才可以打开SlidingMenu
-		// TOUCHMODE_NONE 不能通过手势打开SlidingMenu
-		getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_NONE);
-		getSlidingMenu().setBehindWidth(getResources().getDimensionPixelSize(R.dimen.cliding_menu_behind_width));
-	}
+  private void initSlidingMenu() {
 
-	private void initLeftMenuFragment() {
-		leftMenuFragment.setMenuListOnClickListener(new LeftMenuFragment.MenuListOnItemClickListener() {
+    // 自定义SlidingMenu
+    // 设置只有左侧可以划出,如果需要设置左右都能划LEFT_RIGHT
+    getSlidingMenu().setMode(SlidingMenu.LEFT);
+    // 设置阴影图片
+    getSlidingMenu().setShadowDrawable(R.drawable.bg_sliding_menu_shadow);
+    // 设置阴影图片的宽度
+    getSlidingMenu().setShadowWidthRes(R.dimen.shadow_width);
+    // SlidingMenu划出时主页面显示的剩余宽度
+    getSlidingMenu().setBehindOffsetRes(R.dimen.slidingmenu_offset);
+    // 设置SlidingMenu划出淡入淡出的褪色程度0.0f-1.0f
+    getSlidingMenu().setFadeDegree(0.35f);
+    // 设置SlidingMenu 的手势模式
+    // TOUCHMODE_FULLSCREEN 全屏模式，在整个content页面中，滑动，可以打开SlidingMenu
+    // TOUCHMODE_MARGIN
+    // 边缘模式，在content页面中，如果想打开SlidingMenu,你需要在屏幕边缘滑动才可以打开SlidingMenu
+    // TOUCHMODE_NONE 不能通过手势打开SlidingMenu
+    getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_NONE);
+    getSlidingMenu().setBehindWidth(getResources().getDimensionPixelSize(R.dimen.cliding_menu_behind_width));
+  }
 
-			@Override
-			public void onItemClick(int position) {
-				if (position != 0) {
-					if (!GlobalDataCacheForMemorySingleton.getInstance.isLogged()) {
+  private void initLeftMenuFragment() {
+    leftMenuFragment.setMenuListOnClickListener(new LeftMenuFragment.MenuListOnItemClickListener() {
 
-					}
-				}
+      @Override
+      public void onItemClick(LeftMenuIndexEnum indexEnum) {
+        // 在切换左侧滑栏的菜单之前, 要先判断是否需要先登录
+        if (isNeedToFirstLogin(indexEnum)) {
+          // 如果需要先登录, 就跳转到登录界面
+          gotoLoginActivity();
+          return;
+        }
 
-				Fragment fragment = null;
-				switch (position) {
-				case 0:
-					fragment = new DiscountFragment();
-					break;
-				case 1:
-					fragment = new OrderFragment();
-					break;
-				case 2:
-					fragment = new FavorFragment();
-					break;
-				case 3:
-					fragment = new NotificationFragment();
-					break;
-				case 4:
-					fragment = new SettingFragment();
-					break;
-				default:
-					break;
-				}
+        // 切换左侧滑栏的菜单
+        changeLeftMenuByIndex(indexEnum);
+      }
+    });
+  }
 
-				if (fragment != null) {
-					final FragmentManager fragmentManager = getSupportFragmentManager();
-					FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-					fragmentTransaction.replace(R.id.content, fragment);
-					// 这个是给Fragment添加回退栈
-					fragmentTransaction.addToBackStack(null);
-					fragmentTransaction.commit();
-					getSlidingMenu().showContent();
-				} else {
-					Log.e("MainActivity", "Error in creating fragment");
-				}
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    // 设置前景视图
+    setContentView(R.layout.main_activity_foreground_fragment_container);
+    // 设置背景视图
+    setBehindContentView(R.layout.main_activity_background_fragment_container);
+    
+    initSlidingMenu();
+    initLeftMenuFragment();
 
-			}
+    // 设置 SlidingMenu 内容
+    FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+    fragmentTransaction.replace(R.id.left_menu, leftMenuFragment);
+    fragmentTransaction.replace(R.id.content, new DiscountFragment());
+    fragmentTransaction.commit();
+  }
 
-		});
-	}
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+    case android.R.id.home:
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main_layout);
+      toggle(); // 动态判断自动关闭或开启SlidingMenu
+      return true;
+    default:
+      return super.onOptionsItemSelected(item);
+    }
 
-		initSlidingMenu();
-		initLeftMenuFragment();
+  }
 
-		// 设置 SlidingMenu 内容
-		FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-		fragmentTransaction.replace(R.id.left_menu, leftMenuFragment);
-		fragmentTransaction.replace(R.id.content, new DiscountFragment());
-		fragmentTransaction.commit();
+  public void showMenuContent() {
+    if (getSlidingMenu() != null) {
+      getSlidingMenu().showMenu();
+    }
+  }
 
-	}
+  /**
+   * 判断是否需要先登录
+   * 
+   * @param indexEnum
+   */
+  private boolean isNeedToFirstLogin(LeftMenuIndexEnum indexEnum) {
+    if (indexEnum == LeftMenuIndexEnum.Setting || indexEnum == LeftMenuIndexEnum.AllDiscount) {
+      if (!GlobalDataCacheForMemorySingleton.getInstance.isLogged()) {
+        return true;
+      }
+    }
+    return false;
+  }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case android.R.id.home:
+  private Fragment getLeftMenuFragmentByIndex(LeftMenuIndexEnum indexEnum) throws IllegalArgumentException {
+    if (indexEnum == null) {
+      throw new IllegalArgumentException("入参 indexEnum 为null.");
+    }
+    switch (indexEnum) {
+    case AllDiscount:// 全部折扣
+      return new DiscountFragment();
+    case MyOrder:// 我的订单
+      return new OrderFragment();
+    case MyFavor:// 我的收藏
+      return new FavorFragment();
+    case MyNotification:// 我的提醒
+      return new NotificationFragment();
+    case Setting:// 更多设置
+      return new SettingFragment();
+    default:
+      throw new IllegalArgumentException("入参 indexEnum 无效!");
+    }
+  }
 
-			toggle(); // 动态判断自动关闭或开启SlidingMenu
-			return true;
-		default:
-			return super.onOptionsItemSelected(item);
-		}
+  /**
+   * 切换左侧滑出菜单中包含的界面
+   * 
+   * @param indexEnum
+   */
+  private void changeLeftMenuByIndex(LeftMenuIndexEnum indexEnum) {
+    try {
+      final Fragment nextFragment = getLeftMenuFragmentByIndex(indexEnum);
+      final FragmentManager fragmentManager = getSupportFragmentManager();
+      final FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+      fragmentTransaction.replace(R.id.content, nextFragment);
+      // 这个是给Fragment添加回退栈
+      fragmentTransaction.addToBackStack(null);
+      fragmentTransaction.commit();
+    } catch (IllegalArgumentException e) {
+      DebugLog.e(TAG, e.getLocalizedMessage());
+    } finally {
+      getSlidingMenu().showContent();
+    }
+  }
 
-	}
-
-	public void showMenuContent() {
-		if (getSlidingMenu() != null) {
-			getSlidingMenu().showMenu();
-		}
-	}
+  private void gotoLoginActivity() {
+    Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+    startActivityForResult(intent, IntentRequestCodeEnum.TO_LOGIN_ACTIVITY.ordinal());
+  }
 
 }
--- book-xx-android_sdk3.0/src/main/java/cn/lvyou/fragment/LeftMenuFragment.java
+++ book-xx-android_sdk3.0/src/main/java/cn/lvyou/fragment/LeftMenuFragment.java
@@ -14,11 +14,24 @@ import cn.lvyou.activity.R;
  * 
  */
 public class LeftMenuFragment extends Fragment {
+  public enum LeftMenuIndexEnum {
+    // 全部折扣
+    AllDiscount,
+    // 我的订单
+    MyOrder,
+    // 我的收藏
+    MyFavor,
+    // 我的提醒
+    MyNotification,
+    // 更多设置
+    Setting
+  }
+
   /**
    * 左侧菜单 点击回调接口
    */
   public interface MenuListOnItemClickListener {
-    public void onItemClick(int position);
+    public void onItemClick(LeftMenuIndexEnum indexEnum);
   }
 
   private MenuListOnItemClickListener mOnItemClickListener;
@@ -31,7 +44,7 @@ public class LeftMenuFragment extends Fragment {
 
     @Override
     public void onClick(View v) {
-      mOnItemClickListener.onItemClick((Integer) v.getTag());
+      mOnItemClickListener.onItemClick((LeftMenuIndexEnum) v.getTag());
     }
   };
 
@@ -39,21 +52,26 @@ public class LeftMenuFragment extends Fragment {
   public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
     final View rootView = inflater.inflate(R.layout.fragment_menu, null);
+    // 全部折扣
     final View allDiscount = rootView.findViewById(R.id.btn_all_deal);
     allDiscount.setOnClickListener(mOnClickListener);
-    allDiscount.setTag(0);
+    allDiscount.setTag(LeftMenuIndexEnum.AllDiscount);
+    // 我的订单
     final View myOrder = rootView.findViewById(R.id.btn_my_order);
     myOrder.setOnClickListener(mOnClickListener);
-    myOrder.setTag(1);
+    myOrder.setTag(LeftMenuIndexEnum.MyOrder);
+    // 我的收藏
     final View myFavor = rootView.findViewById(R.id.btn_my_favor);
     myFavor.setOnClickListener(mOnClickListener);
-    myFavor.setTag(2);
+    myFavor.setTag(LeftMenuIndexEnum.MyFavor);
+    // 我的提醒
     final View myNotification = rootView.findViewById(R.id.btn_my_notification);
     myNotification.setOnClickListener(mOnClickListener);
-    myNotification.setTag(3);
+    myNotification.setTag(LeftMenuIndexEnum.MyNotification);
+    // 更多设置
     final View setting = rootView.findViewById(R.id.btn_more_setting);
     setting.setOnClickListener(mOnClickListener);
-    setting.setTag(4);
+    setting.setTag(LeftMenuIndexEnum.Setting);
     return rootView;
   }
 
