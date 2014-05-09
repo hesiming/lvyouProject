37a95db54079ffd3d5b0c163832f652b39978e21
zhihuatang <zhihuatang@192.168.1.106>
2014-05-09 12:54:37 +0800

    合并冲突

---
+++ book-xx-lib/src/main/java/cn/lvyou/domainbean_model/discount_list/DiscountListDonainBeanToolsFactory.java
@@ -0,0 +1,25 @@
+package cn.lvyou.domainbean_model.discount_list;
+
+import cn.lvyou.global_data_cache.UrlConstantForThisProject;
+import cn.lvyou.my_network_engine.domainbean_tools.IDomainBeanAbstractFactory;
+import cn.lvyou.my_network_engine.domainbean_tools.IParseDomainBeanToDataDictionary;
+import cn.lvyou.my_network_engine.domainbean_tools.IParseNetRespondDataToDomainBean;
+
+public final class DiscountListDonainBeanToolsFactory implements IDomainBeanAbstractFactory {
+
+  @Override
+  public IParseDomainBeanToDataDictionary getParseDomainBeanToDDStrategyObject() {
+    return new DiscountListParseDomainBeanToDD();
+  }
+
+  @Override
+  public IParseNetRespondDataToDomainBean getParseNetRespondDataToDomainBeanStrategyObject() {
+    return new DiscountListParseNetRespondStringToDomainBean();
+  }
+
+  @Override
+  public String getSpecialPath(Object netRequestDomainBean) {
+    // 折扣列表
+    return UrlConstantForThisProject.kUrlConstant_SpecialPath_GET_LASTMINUTE_LIST;
+  }
+}
