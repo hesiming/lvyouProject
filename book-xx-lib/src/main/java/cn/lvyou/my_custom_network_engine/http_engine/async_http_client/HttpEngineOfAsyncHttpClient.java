7efcc5e63f88731258494236cb6cdf0a05a1b9fb
tangzhihua <3252475@qq.com>
2014-06-22 23:30:11 +0800

    格式化代码

--- book-xx-lib/src/main/java/cn/lvyou/my_custom_network_engine/http_engine/async_http_client/HttpEngineOfAsyncHttpClient.java
+++ book-xx-lib/src/main/java/cn/lvyou/my_custom_network_engine/http_engine/async_http_client/HttpEngineOfAsyncHttpClient.java
@@ -6,7 +6,6 @@ import java.util.Map;
 import org.apache.http.Header;
 
 import android.text.TextUtils;
-import cn.lvyou.global_data_cache.GlobalDataCacheForMemorySingleton;
 import cn.lvyou.my_network_engine.INetRequestHandle;
 import cn.lvyou.my_network_engine.engine_helper.EngineHelperSingleton;
 import cn.lvyou.my_network_engine.http_engine.IDomainBeanRequestAsyncHttpResponseListener;
@@ -19,7 +18,6 @@ import cn.lvyou.toolutils.ToolsFunctionForThisProgect;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.MySSLSocketFactory;
-import com.loopj.android.http.PersistentCookieStore;
 import com.loopj.android.http.RequestHandle;
 import com.loopj.android.http.RequestParams;
 
@@ -35,12 +33,12 @@ public class HttpEngineOfAsyncHttpClient implements IHttpRequestForDomainBean, I
     // PersistentCookieStore类用于实现Apache HttpClient的CookieStore接口，
     // 可以自动的将cookie保存到Android设备的SharedPreferences中，如果你打算使用cookie来管理验证会话，
     // 这个非常有用，因为用户可以保持登录状态，不管关闭还是重新打开你的app
-    //PersistentCookieStore myCookieStore = new PersistentCookieStore(GlobalDataCacheForMemorySingleton.getInstance.getApplication());
+    // PersistentCookieStore myCookieStore = new
+    // PersistentCookieStore(GlobalDataCacheForMemorySingleton.getInstance.getApplication());
   }
 
   @Override
-  public INetRequestHandle requestDomainBean(final String url, final Map<String, String> dataDictionary,
-      final IDomainBeanRequestAsyncHttpResponseListener domainBeanRequestAsyncHttpResponseListener) {
+  public INetRequestHandle requestDomainBean(final String url, final Map<String, String> dataDictionary, final IDomainBeanRequestAsyncHttpResponseListener domainBeanRequestAsyncHttpResponseListener) {
     DebugLog.i(TAG, "短连接 : sendHttpRequest->url=" + url + ",dataDictionary=" + dataDictionary);
     RequestHandle requestHandle = null;
 
@@ -64,15 +62,14 @@ public class HttpEngineOfAsyncHttpClient implements IHttpRequestForDomainBean, I
 
     // 如果dataDictionary不为空, 我们就使用 POST方式发送数据到服务器, 否则就是用GET方式访问服务器
     if (dataDictionary != null && dataDictionary.size() > 0) {
-      RequestParams requestParams =
-          (RequestParams) EngineHelperSingleton.getInstance.getNetRequestEntityDataPackageStrategyObject().packageNetRequestEntityData(
-              dataDictionary);
+      RequestParams requestParams = (RequestParams) EngineHelperSingleton.getInstance.getNetRequestEntityDataPackageStrategyObject().packageNetRequestEntityData(dataDictionary);
       requestHandle = asyncHttpClient.post(url, requestParams, responseHandler);
     } else {
       requestHandle = asyncHttpClient.get(url, responseHandler);
     }
 
-    // 因为发现AsyncHttpClient原生的 HttpRequestHandle的isFinished标志位有所异常（开始任务时便默认为true），所以在此做一个自定义分装
+    // 因为发现AsyncHttpClient原生的
+    // HttpRequestHandle的isFinished标志位有所异常（开始任务时便默认为true），所以在此做一个自定义分装
     HttpRequestHandleOfAsyncHttpClient httpRequestHandleOfAsyncHttpClient = (HttpRequestHandleOfAsyncHttpClient) responseHandler;
     httpRequestHandleOfAsyncHttpClient.setRequestHandle(requestHandle);
     httpRequestHandleOfAsyncHttpClient.setFinished(false);
@@ -80,8 +77,8 @@ public class HttpEngineOfAsyncHttpClient implements IHttpRequestForDomainBean, I
   }
 
   @Override
-  public INetRequestHandle requestFile(final String url, final boolean isNeedContinuingly, final Map<String, String> dataDictionary,
-      final File downLoadFile, final IFileRequestAsyncHttpResponseListener fileRequestAsyncHttpResponseListener) {
+  public INetRequestHandle requestFile(final String url, final boolean isNeedContinuingly, final Map<String, String> dataDictionary, final File downLoadFile,
+      final IFileRequestAsyncHttpResponseListener fileRequestAsyncHttpResponseListener) {
     DebugLog.i(TAG, "文件请求 : sendHttpRequest->url=" + url + ",dataDictionary=" + dataDictionary);
 
     // effective for java 38 检查参数有效性, 对于共有的方法,
@@ -105,30 +102,27 @@ public class HttpEngineOfAsyncHttpClient implements IHttpRequestForDomainBean, I
     }
 
     try {
-      final RandomAccessFileAsyncHttpResponseHandler fileAsyncHttpResponseHandler =
-          new RandomAccessFileAsyncHttpResponseHandler(downLoadFile, seekPos) {
+      final RandomAccessFileAsyncHttpResponseHandler fileAsyncHttpResponseHandler = new RandomAccessFileAsyncHttpResponseHandler(downLoadFile, seekPos) {
 
-            @Override
-            public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
-              fileRequestAsyncHttpResponseListener.onFailure(statusCode, e);
-            }
+        @Override
+        public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
+          fileRequestAsyncHttpResponseListener.onFailure(statusCode, e);
+        }
 
-            @Override
-            public void onProgress(int bytesWritten, int totalSize) {
-              fileRequestAsyncHttpResponseListener.onProgress(bytesWritten, totalSize);
-            }
+        @Override
+        public void onProgress(int bytesWritten, int totalSize) {
+          fileRequestAsyncHttpResponseListener.onProgress(bytesWritten, totalSize);
+        }
 
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, File file) {
-              fileRequestAsyncHttpResponseListener.onSuccess(file);
-            }
+        @Override
+        public void onSuccess(int statusCode, Header[] headers, File file) {
+          fileRequestAsyncHttpResponseListener.onSuccess(file);
+        }
 
-          };
+      };
       // 如果dataDictionary不为空, 我们就使用 POST方式发送数据到服务器, 否则就是用GET方式访问服务器
       if (dataDictionary != null && dataDictionary.size() > 0) {
-        RequestParams requestParams =
-            (RequestParams) EngineHelperSingleton.getInstance.getNetRequestEntityDataPackageStrategyObject().packageNetRequestEntityData(
-                dataDictionary);
+        RequestParams requestParams = (RequestParams) EngineHelperSingleton.getInstance.getNetRequestEntityDataPackageStrategyObject().packageNetRequestEntityData(dataDictionary);
         requestHandle = asyncHttpClient.post(url, requestParams, fileAsyncHttpResponseHandler);
       } else {
         requestHandle = asyncHttpClient.get(url, fileAsyncHttpResponseHandler);
